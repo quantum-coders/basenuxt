cac283a1c1c2798cccc3d641c9e36e350153c63b
0xjesus <pdlgmcn@protonmail.com>
2024-07-23 20:14:51 -0600

    ✨ (WalletConnect.vue): add authentication logic when a wallet is connected to the app 📝 (authStore.js): create authStore to store user information after successful authentication

--- components/Solana/WalletConnect.vue
+++ components/Solana/WalletConnect.vue
@@ -1,34 +1,51 @@
 <template>
-	<wallet-multi-button />
+	<wallet-multi-button/>
 </template>
 
 <script setup>
-	import { useWallet, initWallet } from 'solana-wallets-vue';
-	import { WalletMultiButton } from 'solana-wallets-vue';
-	const solanaStore = useSolanaStore();
-	const config = useRuntimeConfig();
+import {useWallet, initWallet} from 'solana-wallets-vue';
+import {WalletMultiButton} from 'solana-wallets-vue';
 
-	initWallet({ autoConnect: true });
+const solanaStore = useSolanaStore();
+const config = useRuntimeConfig();
+const authStore = useAuthStore();
+initWallet({autoConnect: true});
 
-	const connectedWallet = computed(() => {
-		const { publicKey, sendTransaction } = useWallet();
+const connectedWallet = computed(() => {
+	const {publicKey, sendTransaction} = useWallet();
 
-		if(publicKey && publicKey.value) {
-			solanaStore.connectedWallet = publicKey.value.toBase58();
-			return publicKey.value.toBase58();
+	if (publicKey && publicKey.value) {
+		solanaStore.connectedWallet = publicKey.value.toBase58();
+		return publicKey.value.toBase58();
+	}
+
+	solanaStore.connectedWallet = null;
+	return null;
+});
+
+watch(connectedWallet, async (currentValue) => {
+	if (currentValue) {
+		const connectRes = await fetch(`${config.public.baseURL}/users/authenticate`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				wallet: connectedWallet.value,
+			}),
+		});
+
+		if (connectRes.ok) {
+			const connectData = await connectRes.json();
+
+			// save accessToken to localStorage
+			localStorage.setItem('accessToken', connectData.accessToken);
+			authStore.user = connectData.data
 		}
-
-		solanaStore.connectedWallet = null;
-		return null;
-	});
-
-	watch(connectedWallet, async (currentValue) => {
-		if(currentValue) {
-
-		} else {
-			localStorage.removeItem('accessToken');
-		}
-	});
+	} else {
+		localStorage.removeItem('accessToken');
+	}
+});
 </script>
 
 <style lang="sass" scoped>
