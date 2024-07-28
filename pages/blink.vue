6cff6cb0ba5f954def452f17a936e782aefda2ff
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-07-27 23:49:13 -0600

    ✨ Add z-index to blink.vue
    This commit adds z-index properties to the blink.vue file, ensuring that the logo-wrapper and main-content sections are properly layered on top of each other.

--- pages/blink.vue
+++ pages/blink.vue
@@ -34,6 +34,7 @@
 		top: 0
 		left: 0
 		width: 100%
+		z-index: 1000
 
 		.logo-wrapper
 			display: flex
@@ -70,4 +71,5 @@
 		justify-content: center
 		align-items: center
 		flex-grow: 1
+		z-index: 1
 </style>
